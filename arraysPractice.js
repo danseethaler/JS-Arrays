c59b957dea330ea6758b50b3cbe3a9ddbf021748
Dan Seethaler <dan@seethalers.com>
2015-07-11 12:38:13 -0600

    Another two down.

--- arraysPractice.js
+++ arraysPractice.js
@@ -129,7 +129,15 @@ var str = 'this is my sentence';
 //Write a function called reverse that takes a given str as it's only argument and returns that string after it's been reversed
 
   //Code Here
+function reverse(str) {
+    var reversedString;
+    for (var i = str.length - 1; i >= 0; i--) {
+        reversedString += str[i];
+    }
+    return reversedString;
+}
 
+reverse(str);
 
 //Next Problem
 
